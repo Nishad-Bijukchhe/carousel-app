574106931d961cfbec660838b9607ec4189c1ee7
Nishad-Bijukchhe <nishad.bijukchhe1@gmail.com>
2025-02-21 06:58:05 +0545

    project init

---
+++ src/components/layout/layout.tsx
@@ -0,0 +1,15 @@
+import "../style/globals.css";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={"bg-white text-black"}>
+        {children}
+      </body>
+    </html>
+  );
+}
